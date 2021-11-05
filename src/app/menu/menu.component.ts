470c9d146bc7ddc654c509132452ea2f738a3ecd
Vikram Kadiam <vikraal10@gmail.com>
2021-11-05 20:58:43 +0000

    feat(animation):added few features

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,11 +3,39 @@ import { Component, VERSION, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { SharedService } from '../services/shared.service';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  animations: [
+    // animation triggers go here
+    // ...
+    trigger('openClose', [
+      state('open', style({
+        height: '50px',
+        opacity: 1,
+        backgroundColor: 'yellow',
+        color: 'black'
+      })),
+      state('closed', style({
+        height: '50px',
+        opacity: 0.8,
+        backgroundColor: '#c6ecff'
+      })), transition('open => closed', [
+        animate('0.5s')
+      ]), transition('closed => open', [
+        animate('0.5s')
+      ])
+    ]),
+  ]
 })
 export class MenuComponent {
 
